cc5d03cf02b51997c34a059c8ea860087f65037f
masaki.arizuka <masaki.arizuka@gmail.com>
2018-08-12 14:59:23 +0900

    Update export setting

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 const fs = require('fs');
 const withSass = require('@zeit/next-sass')
+
 const getRoutes = async () => {
+  const pathMap = {
+    '/':  { page: '/index' },
+    '/posts':  { page: '/posts' },
+  };
+
   const posts = JSON.parse(fs.readFileSync('./static/posts.json', 'utf-8'));
   return posts.reduce((map, post) => {
     map[`/posts/${post.href}`] = { page: '/_post_content', query: { id: post.href } }
     return map;
-  }, {});
+  }, pathMap);
 };
 
 const config = {
