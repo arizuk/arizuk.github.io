79da87a7ef70b7b4e64fab335e7d2eb167529379
masaki.arizuka <masaki.arizuka@gmail.com>
2020-05-22 21:15:23 +0900

    Fix react warnings

--- components/layout.js
+++ components/layout.js
@@ -7,10 +7,10 @@ const GA_TRACKING_ID = "UA-131422067-1"
 export default (props) => (
   <div>
     <Head>
-      <meta charset="utf-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous" />
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossOrigin="anonymous" />
       <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/default.min.css" />
       <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico" />
       <title>{props.title || 'arizuk.github.io'}</title>
